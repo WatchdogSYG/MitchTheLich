26368f5faaf5b71abbd7b6e9efe9ef0a2e8a301c
Brandon Lu <sfxninja183@hotmail.com>
2018-09-01 16:28:18 +0930

    Camera following and cleanup
    Camera now tracks the player. Feel free to tune and play around with angles and distances in the gamevariables class. Cleaned up asset directory.

--- mtl/Assets/Scripts/GameVariables.cs
+++ mtl/Assets/Scripts/GameVariables.cs
@@ -1,7 +1,16 @@
 ﻿// MDT This script is for placing game-gobal variables for tuning and moularization.
-//May want to enable word wrap...
+//May want to enable word wrap for long descriptions...
 
 namespace mtl {
+
+	public class Camera {
+		public const float ISO_PITCH = 60f;//degrees downwards around the x axis for this isometric camera
+		public const float ISO_YAW = 0f;//degrees around the y axis for this isometric camera
+		public const float ISO_ROLL = 0f;//degrees around the z axis for this isometric camera
+		public const float CAM_DISPLACEMENT = 10f;//metres from focus target (for lighting and near-clip plane modifications only)
+		public const float ORTHO_HEIGHT = 15f;//length of the orthographic half-box height
+	}
+
 
     public class Player {
             public const float PLAYER_BASE_MOVE_SPEED = 15f; //base player move speed. All other movement variables should be a function of this variable.
---
+++ mtl/Assets/Scripts/move/cameraFollow.cs
@@ -0,0 +1,39 @@
+﻿//MDT_Brandon startContribution
+//Camera follows the first object with the "Player" tag at a distance of mtl.Camera.CAM_DISPLACEMENT metres away with an orthographic size of ORTHO_HEIGHT. Works with any camera rotation.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using mtl;
+
+public class cameraFollow : MonoBehaviour {
+	//returns a string displaying the xyz components of a Vector3 (may be moved into a lib later)
+	string Vector3Log(Vector3 v, int sigFig) {
+		string vString = "(" +
+			v.x.ToString("F" + sigFig.ToString()) + "x, " +
+			v.y.ToString("F" + sigFig.ToString()) + "y, " +
+			v.z.ToString("F" + sigFig.ToString()) + "z)";
+		return vString;
+	}
+
+	Vector3 worldDefaultUnitVector = new Vector3(0, 0, 1);//a vector with no rotation points in the z direction
+	Vector3 playerInitialPos, unitPlayerToCam;
+
+	// Use this for initialization
+	void Start() {
+		//set initial camera rotation and size
+		UnityEngine.Camera.main.orthographicSize = mtl.Camera.ORTHO_HEIGHT;
+		Quaternion rotation = Quaternion.Euler(mtl.Camera.ISO_PITCH, mtl.Camera.ISO_YAW, mtl.Camera.ISO_ROLL);
+		UnityEngine.Camera.main.transform.rotation = rotation;
+		//give a unit vector in the direction of the player facing the camera
+		unitPlayerToCam = rotation * new Vector3(0, 0, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//find the player's current position
+		playerInitialPos = GameObject.FindWithTag("Player").transform.position;
+		//set the camera so that it faces the player at a distance of CAM_DISPLACEMENT
+		UnityEngine.Camera.main.transform.position = playerInitialPos + (mtl.Camera.CAM_DISPLACEMENT * Vector3.Normalize(-unitPlayerToCam));
+	}
+}
+//MDT_Brandon endContribution
