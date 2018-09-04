1c1071a66c0cd80abd29141e45b4457d2f0a0c85
Seth Touchette <seth.touchette@gmail.com>
2018-09-04 17:57:30 +0930

    Animation - 1
    Currently working with the idle animation - Mitch obj in Environment Extended scene idles but the model is crumbled into itself. Not sure whats going on here. Refer with Jake.

---
+++ mtl/Assets/Scripts/Animation/MovementAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAnimation : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.SetBool("gameHasStarted", true);
+	}
+
+    private void Update()
+    {
+        CallingAnimations();
+    }
+
+    void CallingAnimations()
+    {
+        if (Input.GetKeyDown("a") || Input.GetKeyDown("d"))
+        {          
+            anim.SetBool("movementWithHorizontal", true);
+        }
+        //if (Input.GetKeyUp("a") || Input.GetKeyDown("d"))
+        //{
+        //    anim.SetBool("", true); //Make another transition bool into idle and it will call the idle animation when the player stops pressing the key.
+        //}
+    }
+
+}
