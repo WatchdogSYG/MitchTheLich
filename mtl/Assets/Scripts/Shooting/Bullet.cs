3cc64149b10e1d9c8073d5bc01377585585dd915
THEBIG0 <ninjamonkeysogek@gmail.com>
2018-09-06 17:37:38 +0930

    Mitch Can Shoot and bullet is destroyed after 2 seconds
    Mitch Can Shoot and bullet is destroyed after 2 seconds and documentation provided

---
+++ mtl/Assets/Scripts/Shooting/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    //Author: Owen.Gunter
+    //Purpose: to give properties to bullet
+    //Date: 06/09/2018
+
+    // Use this for initialization
+
+    public float MaxLifeTime = 2f;
+	void Start () {
+        // if it isnt already destroyed kill object after 2 seconds
+        Destroy(gameObject, MaxLifeTime);
+		
+	}
+	
+	
+	void OnCollisionEnter (Collision other)
+    {
+        Destroy(gameObject);
+	}
+}
