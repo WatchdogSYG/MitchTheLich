18d7e06cfef32cf3192b0b2ce7dc46f33106be0e
Seth Touchette <seth.touchette@gmail.com>
2018-10-17 13:20:47 +1030

    CollisionUpdate1
    colliders have been remade on some parts of terrain. CollisionUpdate2 will finish the rest of it.

---
+++ mtl/Assets/Scripts/Dialogue/DialogueCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueCollider : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "TriggerText1")
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("fuckFlowersText");
+        }
+    }
+}
