ae0b633a8eda9990776c652de41b5d507ab0a922
Wackers01 <42710984+Wackers01@users.noreply.github.com>
2018-10-23 18:00:05 +1030

    Cutscene
    Added New Scene, Script and Cinemachine

---
+++ mtl/Assets/Scripts/ui/Actionbar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+//Author Timothy Waclawik 
+//Used to make actionbar switch between fire and ice spells
+public class Actionbar : MonoBehaviour {
+    Canvas canvas;
+
+
+    // Use this for initialization
+    void Start () {
+        canvas = GetComponent<Canvas>();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //ActionBar UI element that is hidden and switches to fire spell images on actionbar when this button is pressed
+        if (Input.GetButtonDown(("Element1")))
+        {
+            canvas.enabled = !canvas.enabled;
+        }
+
+    }
+}
---
+++ mtl/Assets/Scripts/ui/LoadOnActivation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnActivation : MonoBehaviour {
+
+    private void OnEnable()
+    {
+        SceneManager.LoadScene("mtl_01", LoadSceneMode.Single);
+    }
+}
